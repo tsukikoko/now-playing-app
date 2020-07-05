453e80c000e8ef9adf062f7d70c052779ad29b42
Tsukiko Tahara <tsukiko.works@gmail.com>
2020-07-06 00:02:17 +0900

    update プレイリストのコンポーネント化

---
+++ src/components/MyPlayList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container">
+    <h2>{{ playListName }}</h2>
+    <ul>
+      <li v-for="(value, index) in playList" :key="index">
+        <dl>
+          <dt>track_id</dt>
+          <dd>{{ playList[index].track_id }}</dd>
+          <dt>曲名</dt>
+          <dd>{{ playList[index].song_name }}</dd>
+          <dt>アーティスト名</dt>
+          <dd>{{ playList[index].artist_name }}</dd>
+        </dl>
+        song_name {{ playList[index].song_name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+ul {
+  list-style: none;
+  text-align: left;
+}
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      playListName: "",
+      index: 0,
+      data: [],
+      playList: [],
+      song: [],
+    };
+  },
+  created: function() {
+    let endpoint =
+      "https://api.spotify.com/v1/playlists/3203elB6L86rNk3TMqcTjU";
+    let access_token =
+      "BQD5jcdW1qwsrlrRWpPxTaulJ1FWoEHHSw3An9VNTYiZjkPJCymQSVImwMybTRahLxVpoVn9ILrBlOKGlOEdVJ2cGuTa-OEHFbUBvs7aLtM1qB5sD_KFs2GkKdKS9_Rg5e_9LaztfK-g-Q6fxRQEDtjC4R_RpngcryRdL2aA";
+    axios
+      .get(endpoint, {
+        headers: {
+          Authorization: "Bearer " + access_token,
+        },
+      })
+      .then((response) => {
+        const length = response.data.tracks.items.length; // 曲数
+        this.data = response.data.tracks;
+        this.playListName = response.data.name;
+
+        for (let i = 0; i < length; i++) {
+          this.song = {
+            track_id: this.data.items[i].track.id,
+            song_name: this.data.items[i].track.name,
+            track_img_url: this.data.items[i].track.album.images[0].url,
+            artist_name: this.data.items[i].track.artists[0].name,
+            song_uri: this.data.items[i].track.href,
+          };
+          this.playList.id;
+          this.playList = this.playList.concat(this.song);
+        }
+      })
+      .catch((error) => console.log(error));
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,71 +1,14 @@
 <template>
-  <div class="container">
-    <h2>{{ playListName }}</h2>
-    <ul>
-      <li v-for="(value, index) in playList" :key="index">
-        <dl>
-          <dt>track_id</dt>
-          <dd>{{ playList[index].track_id }}</dd>
-          <dt>曲名</dt>
-          <dd>{{ playList[index].song_name }}</dd>
-          <dt>アーティスト名</dt>
-          <dd>{{ playList[index].artist_name }}</dd>
-        </dl>
-
-        song_name {{ playList[index].song_name }}
-      </li>
-    </ul>
-  </div>
+  <myPlayList></myPlayList>
 </template>
 
-<style>
-ul {
-  list-style: none;
-  text-align: left;
-}
-</style>
+<style></style>
 
 <script>
-import axios from "axios";
+import myPlayList from "../components/MyPlayList.vue";
 export default {
-  data() {
-    return {
-      playListName: "",
-      index: 0,
-      data: [],
-      playList: [],
-      song: [],
-    };
-  },
-  created: function() {
-    let endpoint =
-      "https://api.spotify.com/v1/playlists/3203elB6L86rNk3TMqcTjU";
-    let access_token =
-      "BQAqYJQccXlu7IH_ElGCwlGHqeacYl069yiPu8lDSV0XQfvp-jJ6ElgRMo0He3I57CkVuhg4MP4l4wbxs7EpTTvXnMWs38wQ5OQYWeCYUaNZ5PmDYjGW2Px-NKnNsDjZtG_lJy3WJ6ekP9bkWvPdlefkkgzA98iqzBeAAiIcVLID7013qEi2bafLbArEOm_BcxbSF37AUXbAyyoYfrr0mlZhRfvbXLS9J_0oMcbTE1setgwJfq-9G2mi3Pj4L0HaVpUuCUkWbSY23DXXqBpkQZyckY1bZXYq3aab";
-    axios
-      .get(endpoint, {
-        headers: {
-          Authorization: "Bearer " + access_token,
-        },
-      })
-      .then((response) => {
-        const length = response.data.tracks.items.length; // 曲数
-        this.data = response.data.tracks;
-        this.playListName = response.data.name;
-
-        for (let i = 0; i < length; i++) {
-          this.song = {
-            track_id: this.data.items[i].track.id,
-            song_name: this.data.items[i].track.name,
-            track_img_url: this.data.items[i].track.album.images[0].url,
-            artist_name: this.data.items[i].track.artists[0].name,
-            song_uri: this.data.items[i].track.href,
-          };
-          this.playList.id;
-          this.playList = this.playList.concat(this.song);
-        }
-      })
-      .catch((error) => console.log(error));
+  components: {
+    myPlayList,
   },
 };
 </script>
